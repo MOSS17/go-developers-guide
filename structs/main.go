c6db5abdd365c9da3bf57dbab00dfc7bd5abf0ec
Miguel Sandoval <miguel_0799@hotmail.com>
2024-01-04 22:35:17 -0700

    pointers

--- structs/main.go
+++ structs/main.go
@@ -10,18 +10,27 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Jones",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 95200,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
